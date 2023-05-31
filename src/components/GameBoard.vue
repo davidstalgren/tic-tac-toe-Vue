f65a368f50679c84d5dee498be219483f582f5a3
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-05-31 15:30:50 +0200

    add stateOfTheGame and function to add the players

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import AddPlayersForm from './AddPlayersForm.vue';
+import { IStateOfTheGame } from '../models/IStateOfTheGame';
+import { Player } from '../models/Player';
 
-console.log('from gameboard component');
+const stateOfTheGame = ref<IStateOfTheGame>({
+    players: [],
+    board: ['', '', '', '', '', '', '', '', ''],
+    playerTurn: 'X',
+    gameStarted: false
+});
+
+function addNewPlayers(newPlayers: string[]) {
+    console.log('from function addNewPlayers', newPlayers[0] + ' ' + newPlayers[1]);
+    stateOfTheGame.value.players.push(new Player(newPlayers[0], 'X', false));
+    stateOfTheGame.value.players.push(new Player(newPlayers[1], 'O', false));
+    stateOfTheGame.value.gameStarted = true;
+}
+
 </script>
 
 <template>
-    <AddPlayersForm/>
-    <div>Game Board</div>
+    <AddPlayersForm v-if="!stateOfTheGame.gameStarted" @addNewPlayers="addNewPlayers"></AddPlayersForm>
     
+    <div v-if="stateOfTheGame.gameStarted">
+        <p>
+            Test players from player[] {{ stateOfTheGame.players[0].name }} and {{ stateOfTheGame.players[1].name }} is {{ stateOfTheGame.players[0].symbol }} and {{ stateOfTheGame.players[1].symbol }}
+        </p>
+    </div>
 </template>
 
 <style scoped>
